0f3bb055fc90456f21cd3dca988387b90cb8dc31
Andrew Ford <andrew@andrewford.co.nz>
2023-09-09 03:05:07 +0000

    Added routes Removed launch.json

--- src/app/api/items/route.js
+++ src/app/api/items/route.js
@@ -3,8 +3,9 @@ import { NextResponse } from 'next/server'
 export async function GET() {
 
     const data = {
-        id: 123
+        id: 123,
+        name: "item 1"
     }
 
-    return NextResponse.json({ data })
+    return NextResponse.json(data)
 }
